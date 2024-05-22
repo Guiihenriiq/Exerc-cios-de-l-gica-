cb841a626b1e17acc38c4acd4c4a845f003a81f1
Guiihenriiq <henri3985@gmail.com>
2024-05-21 21:15:11 -0300

    Código que inverte a palavra inserida pelo usuário

---
+++ inverte palavra.py
@@ -0,0 +1,4 @@
+palavra = str(input("Digite uma palavra: "))
+print(" A palavra digitada foi {}" ,format(palavra))
+string = palavra[::-1]
+print("A palavra que você inseriu invertida fica: {}",format(string))
