87dd28d10bb70516220d1ebbe6d00ef6b5ffb209
Guiihenriiq <henri3985@gmail.com>
2024-05-22 00:56:15 -0300

    Adicionei a opção de repetir o processo ou finalizar o sistema usando If

--- inverte palavra.py
+++ inverte palavra.py
@@ -1,4 +1,23 @@
+continuar =1
 palavra = str(input("Digite uma palavra: "))
-print(" A palavra digitada foi {}" ,format(palavra))
+print(" A palavra digitada foi " ,format(palavra))
 string = palavra[::-1]
-print("A palavra que você inseriu invertida fica: {}",format(string))
+print("A palavra que você inseriu invertida fica: ",format(string))
+
+continuar = int(input("digite 1 para continuar ou 2 para finalizar: "))
+
+if continuar==1:
+    
+    palavra = str(input("Digite uma palavra: "))
+    print(" A palavra digitada foi " ,format(palavra))
+    string = palavra[::-1]
+    print("A palavra que você inseriu invertida fica: ",format(string))
+    continuar = int(input("digite 1 para continuar ou 2 para finalizar: "))
+
+else:
+    print("Até logo")
+
+
+
+
+
